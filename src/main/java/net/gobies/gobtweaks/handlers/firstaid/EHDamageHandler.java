d445dac0ac979e08bf2b9e512cdb59f0212155b4
gobies <84656386+gobies21@users.noreply.github.com>
2026-05-11 18:04:59 -0400

    Improved enhanced visuals x first aid compat by making armor and enchantments accounted for

--- src/main/java/net/gobies/gobtweaks/handlers/firstaid/EHDamageHandler.java
+++ src/main/java/net/gobies/gobtweaks/handlers/firstaid/EHDamageHandler.java
@@ -3,18 +3,19 @@ package net.gobies.gobtweaks.handlers.firstaid;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import team.creative.enhancedvisuals.EnhancedVisuals;
 
 public class EHDamageHandler {
 
     @SubscribeEvent
-    public static void onLivingHurt(LivingHurtEvent event) {
+    public static void onLivingDamage(LivingDamageEvent event) {
         LivingEntity entity = event.getEntity();
         DamageSource damage = event.getSource();
         if (entity instanceof Player player) {
-            EnhancedVisuals.EVENTS.damage(player, damage, event.getAmount());
+            float amount = event.getAmount();
+            EnhancedVisuals.EVENTS.damage(player, damage, amount);
         }
     }
 }
